d2b0eb3d21b1316abc059cbe99f874f010c1dccd
tglide <26071571+TGlide@users.noreply.github.com>
2024-11-19 00:26:20 +0000

    yay

--- docs/src/previews/popover.svelte
+++ docs/src/previews/popover.svelte
@@ -1,44 +1,43 @@
 <script lang="ts">
-	import Preview, { usePreviewControls } from "@components/preview.svelte";
-	import { Popover } from "melt/components";
+	import Preview from "@components/preview.svelte";
+	import { Popover as PopoverComponent } from "melt/components";
+	import { Popover } from "melt/builders";
 
-	const _controls = usePreviewControls({});
+	const popover = new Popover({
+		forceVisible: true,
+	});
 </script>
 
 <Preview>
-	<Popover forceVisible>
-		{#snippet children(popover)}
-			<button
-				class="mx-auto block rounded-xl bg-gray-800 px-4 py-2 text-gray-200
+	<button
+		class="mx-auto block rounded-xl bg-gray-800 px-4 py-2 text-gray-200
 				 transition-all hover:cursor-pointer hover:bg-gray-500/50
 			  active:bg-gray-600/50 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:opacity-50"
-				{...popover.trigger}
-			>
-				psst...
-			</button>
+		{...popover.trigger}
+	>
+		psst...
+	</button>
 
-			<div class="w-[260px] rounded-2xl bg-gray-800 p-4 shadow-xl" {...popover.content}>
-				<p class="text-center font-semibold">Can I tell you a secret?</p>
+	<div class="w-[260px] rounded-2xl bg-gray-800 p-4 shadow-xl" {...popover.content}>
+		<p class="text-center font-semibold">Can I tell you a secret?</p>
 
-				<div class="mt-4 flex items-center justify-center gap-4">
-					<Popover forceVisible>
-						{#snippet children(popover2)}
-							<button
-								class="border-b-2 border-dashed bg-transparent transition-all hover:cursor-pointer hover:opacity-75 active:opacity-50"
-								{...popover2.trigger}
-							>
-								yes
-							</button>
+		<div class="mt-4 flex items-center justify-center gap-4">
+			<PopoverComponent forceVisible>
+				{#snippet children(popover2)}
+					<button
+						class="border-b-2 border-dashed bg-transparent transition-all hover:cursor-pointer hover:opacity-75 active:opacity-50"
+						{...popover2.trigger}
+					>
+						yes
+					</button>
 
-							<div {...popover2.content} class="rounded-xl bg-gray-700 p-4 shadow-xl backdrop-blur">
-								you're awesome
-							</div>
-						{/snippet}
-					</Popover>
-				</div>
-			</div>
-		{/snippet}
-	</Popover>
+					<div {...popover2.content} class="rounded-xl bg-gray-700 p-4 shadow-xl backdrop-blur">
+						you're awesome
+					</div>
+				{/snippet}
+			</PopoverComponent>
+		</div>
+	</div>
 </Preview>
 
 <style>
