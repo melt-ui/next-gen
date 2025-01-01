64e3b87d6cb632105d99eb39969fb8431ad8aee3
bennymi <xbenmmusicx@gmail.com>
2025-01-01 13:56:35 +0100

    wip

---
+++ docs/src/previews/slider-multi-thumb.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import Preview, { usePreviewControls } from "@components/preview.svelte";
+	import { SliderMultiThumb } from "melt/components";
+
+	const controls = usePreviewControls({
+		step: { type: "number", label: "Step", defaultValue: 1 },
+		min: { type: "number", label: "Min", defaultValue: 0 },
+		max: { type: "number", label: "Max", defaultValue: 100 },
+		orientation: {
+			type: "select",
+			label: "Orientation",
+			options: ["horizontal", "vertical"],
+			defaultValue: "horizontal",
+		},
+	});
+
+	let value = $state([0, 25, 50, 75, 100]);
+</script>
+
+<Preview>
+	<div class="flex flex-col gap-8">
+		<button onclick={() => (value[0] += 10)}>test</button>
+		<div class="text-center">[{value}]</div>
+
+		<SliderMultiThumb bind:value {...controls}>
+			{#snippet children(slider)}
+				<div
+					class="group relative mx-auto p-3 outline-none
+					{slider.orientation === 'horizontal' ? 'w-[350px] max-w-[90%]' : 'h-[350px] w-[50px]'}"
+					{...slider.root}
+				>
+					<div
+						class="absolute rounded-full bg-gray-500
+						{slider.orientation === 'horizontal'
+							? 'left-0 right-0 top-1/2 h-2 -translate-y-1/2'
+							: 'bottom-0 left-1/2 top-0 w-2 -translate-x-1/2'}"
+					>
+						<div
+							class="bg-accent-300 absolute inset-0 rounded-full transition-all group-data-[dragging]:transition-none
+							{slider.orientation === 'horizontal' ? 'right-[var(--percentage-inv)]' : 'top-[var(--percentage)]'}"
+						></div>
+					</div>
+					{#each slider.thumbs as thumb}
+						<div
+							class="focus-visible:ring-accent-300 border-accent-300 absolute size-6 rounded-md border-2 bg-white outline-none transition-all
+							focus-visible:ring focus-visible:ring-offset-black data-[dragging]:transition-none
+							dark:border-none dark:focus-visible:ring-offset-2
+							{slider.orientation === 'horizontal'
+								? 'left-[var(--percentage)] top-1/2 -translate-x-1/2 -translate-y-1/2'
+								: 'left-1/2 top-[var(--percentage)] -translate-x-1/2 -translate-y-1/2'}"
+							{...thumb.trigger}
+						></div>
+					{/each}
+				</div>
+			{/snippet}
+		</SliderMultiThumb>
+	</div>
+</Preview>
--- packages/melt/src/lib/builders/SliderMultiThumb.svelte.ts
+++ packages/melt/src/lib/builders/SliderMultiThumb.svelte.ts
@@ -1,6 +1,9 @@
 
+import { styleAttr } from "$lib/utils/attribute";
 import { extract } from "$lib/utils/extract";
-import type { MaybeGetter } from "../types";
+import { clamp } from "$lib/utils/number";
+import { Synced } from "../Synced.svelte";
+import type { Getter, MaybeGetter } from "../types";
 
 export type SliderMultiThumbProps = {
     /**
@@ -27,8 +30,24 @@ export type SliderMultiThumbProps = {
 	 * @default 1
 	 */
 	step?: MaybeGetter<number | undefined>;
+    /**
+	 * The default value.
+	 *
+	 * When passing a getter, it will be used as source of truth,
+	 * meaning that `value` only changes when the getter returns a new value.
+	 *
+	 * @default undefined
+	 */
+	value?: MaybeGetter<number[] | undefined>;
+    /**
+	 * Called when the `Slider` instance value changes.
+	 */
+	onValueChange?: (active: number[]) => void;
 };
 
+/**
+ * TODO: description
+ */
 export class SliderMultiThumb {
     /* Props */
     #props!: SliderMultiThumbProps;
@@ -38,11 +57,30 @@ export class SliderMultiThumb {
     readonly step = $derived(extract(this.#props.step, 1));
 
     /* State */
-    
+    #value: Synced<number[]>;
 
     constructor(props: SliderMultiThumbProps = {}) {
         this.#props = props;
+        this.#value = new Synced({
+            value: props.value,
+            onChange: props.onValueChange,
+            defaultValue: [0]
+        });
     }
+
+	/** The value of the slider. */
+	get value() {
+		return this.#value.current;
+	}
+
+	set value(value: number[]) {
+		// TODO
+	}
+
+	set valueAtIndex(v: { value: number, index: number }) {
+		const valueFixedToStep = Math.round(v.value / this.step) * this.step;
+		this.#value.current[v.index] = clamp(this.min, valueFixedToStep, this.max);
+	}
 
     /**
 	 * The root of the slider.
@@ -53,4 +91,93 @@ export class SliderMultiThumb {
             "aria-orientation": this.orientation
         };
     }
+
+	get thumbs() {
+		return this.value.map((v, i) => new Thumb({ slider: this, value: () => v, index: i }));
+	}
+}
+
+type ThumbProps = {
+	slider: SliderMultiThumb;
+	value: Getter<number>;
+	index: number;
+}
+
+class Thumb {
+	/* Props */
+	#props!: ThumbProps;
+	slider = $derived(this.#props.slider);
+	index = $derived(this.#props.index);
+	
+	/* State */
+	#value: Synced<number>;
+	// #mouseDown = false;
+	// #dragging = false;
+
+	constructor(props: ThumbProps) {
+		this.#props = props;
+		this.#value = new Synced({
+			value: props.value,
+			defaultValue: 0
+		});
+	}
+
+	get value() {
+		return this.#value.current;
+	}
+
+	set value(value: number) {
+		this.slider.valueAtIndex = { value, index: this.index };
+	}
+
+	get #percentage() {
+		const v = (this.value - this.slider.min) / (this.slider.max - this.slider.min);
+		return this.slider.orientation === "vertical" ? 1 - v : v;
+	}
+
+	get trigger() {
+		return {
+			"aria-valuenow": this.value,
+			"aria-valuemin": this.slider.min,
+			"aria-valuemax": this.slider.max,
+			"aria-orientation": this.slider.orientation,
+			role: "slider",
+			tabindex: 0,
+			id: null,
+			style: styleAttr({
+				[`--percentage`]: `${this.#percentage * 100}%`,
+				[`--percentage-inv`]: `${(1 - this.#percentage) * 100}%`,
+				"touch-action": this.slider.orientation === "vertical" ? "pan-x" : "pan-y"
+			}),
+			onkeydown: (e: KeyboardEvent) => {
+				switch (e.key) {
+					case "ArrowDown":
+					case "ArrowLeft": {
+						if (e.metaKey) this.value = this.slider.min;
+						else this.value -= this.slider.step;
+						break;
+					}
+					case "ArrowUp":
+					case "ArrowRight": {
+						if (e.metaKey) this.value = this.slider.max;
+						else this.value += this.slider.step;
+						break;
+					}
+					case "Home": {
+						this.value = this.slider.min;
+						break;
+					}
+					case "End": {
+						this.value = this.slider.max;
+						break;
+					}
+					default: {
+						return;
+					}
+				}
+
+				e.preventDefault();
+			},
+		};
+	}
 }
