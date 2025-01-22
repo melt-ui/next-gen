9066303c8b885cf66b36d5d8586e6cc20d14b45a
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-01-22 11:40:52 +0000

    style progress preview

--- docs/src/previews/progress.svelte
+++ docs/src/previews/progress.svelte
@@ -2,30 +2,65 @@
 	import NumberFlow from "@number-flow/svelte";
 	import Preview from "@components/preview.svelte";
 	import { Progress } from "melt/builders";
+	import { Spring } from "svelte/motion";
 
-	const progress = new Progress();
-	let value = $derived(Math.round(progress.value));
-
-	const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
-	sleep(1000).then(() => {
-		const intervalId = setInterval(() => {
-			if (progress.value >= 80) clearInterval(intervalId);
-			else progress.value += 0.1;
-		}, 10);
+	const spring = new Spring(Math.round(Math.random() * 100));
+	const progress = new Progress({
+		value: () => spring.current,
 	});
+	const value = $derived(Math.round(progress.value));
+
+	function scaleConvert(value: number, from: [number, number], to: [number, number]) {
+		const [minA, maxA] = from;
+		const [minB, maxB] = to;
+		return ((value - minA) / (maxA - minA)) * (maxB - minB) + minB;
+	}
+
+	function clamp(min: number, value: number, max: number) {
+		return Math.min(Math.max(value, min), max);
+	}
+
+	$effect(() => {
+		progress.value = scaleConvert(value, [0, 100], [1, 0]);
+	});
+
+	$effect(() => {
+		progress.value = scaleConvert(spring.current, [0, 100], [1, 0]);
+	});
+
+	$effect(() => {
+		const interval = setInterval(() => {
+			spring.target = Math.round(Math.random() * 100);
+		}, 2000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
+
+	const h = 34;
+	const maxS = 91.01;
+	const s = $derived(Math.min(scaleConvert(value, [0, 60], [0, maxS]), maxS));
+	const minL = 65.1;
+	const l = $derived(clamp(minL, scaleConvert(value, [0, 80], [100, minL]), 100));
+	const color = $derived(`hsl(${h}, ${s}%, ${l}%)`);
 </script>
 
 <Preview class="place-content-center">
-	<div
-		{...progress.root}
-		class="relative h-6 w-[300px] overflow-hidden rounded-full border border-white"
-	>
-		<div {...progress.progress} class="h-full w-full -translate-x-[var(--progress)] bg-white"></div>
-		<span
-			class="text-accent-500 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold"
-		>
-			<NumberFlow {value} suffix="%" />
+	<div class="flex flex-col items-center gap-2">
+		<span class="origin-bottom" style:scale={scaleConvert(value, [0, 100], [1, 2])} style:color>
+			<NumberFlow {value} suffix="%" class="font-semibold" />
 		</span>
+		<div
+			{...progress.root}
+			class="relative w-[300px] overflow-hidden rounded-full bg-neutral-800"
+			style:height={`${scaleConvert(value, [0, 100], [8, 24])}px`}
+		>
+			<div
+				{...progress.progress}
+				class="h-full w-full -translate-x-[var(--progress)]"
+				style:background-color={color}
+			></div>
+		</div>
 	</div>
 </Preview>
