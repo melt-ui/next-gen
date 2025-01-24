161ab408da3c0f45b53786196e571414e733986c
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-01-24 19:03:02 +0000

    improve

--- docs/src/previews/radio-group.svelte
+++ docs/src/previews/radio-group.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import Preview from "@components/preview.svelte";
-	import { scale } from "svelte/transition";
 	import { RadioGroup } from "melt/builders";
 	import { getters } from "melt/builders";
 	import { usePreviewControls } from "@components/preview-ctx.svelte";
@@ -50,7 +49,7 @@
 <Preview>
 	<div class="mx-auto flex w-fit flex-col gap-2" {...group.root}>
 		<!-- svelte-ignore a11y_label_has_associated_control -- https://github.com/sveltejs/svelte/issues/15067 -->
-		<label {...group.label} class="font-semibold">Layout</label>
+		<label {...group.label} class="font-medium">Layout</label>
 		<div class="flex {isVert ? 'flex-col gap-1' : 'flex-row gap-3'}">
 			{#each items as i}
 				{@const item = group.getItem(i)}
@@ -60,18 +59,24 @@
 					{...item.attrs}
 				>
 					<div
-						class="grid h-6 w-6 place-items-center
-							rounded-full bg-white shadow-sm hover:bg-gray-100 data-[disabled=true]:bg-gray-400"
+						class={[
+							"grid h-6 w-6 place-items-center rounded-full border shadow-sm",
+							"hover:bg-gray-100 data-[disabled=true]:bg-gray-400",
+							item.checked
+								? "bg-accent-500 border-accent-500 dark:bg-white"
+								: "border-neutral-400 bg-neutral-100",
+							"dark:border-white",
+						]}
 					>
 						{#if item.checked}
 							<div
-								transition:scale={{ duration: 150, opacity: 1 }}
-								class="bg-accent-500 h-3 w-3 rounded-full"
+								class={["h-3 w-3 rounded-full", item.checked && "dark:bg-accent-500 bg-white"]}
+								aria-hidden="true"
 							></div>
 						{/if}
 					</div>
 
-					<span class="font-medium capitalize leading-none dark:text-gray-100 text-gray-600">
+					<span class="font-semibold capitalize leading-none text-gray-600 dark:text-gray-100">
 						{i}
 					</span>
 				</div>
