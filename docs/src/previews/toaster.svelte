0ae69f735311970fc487fc8f5a15d6c8220cf303
bennymi <xbenmmusicx@gmail.com>
2025-01-25 13:25:29 +0100

    update documentation and add changeset

--- docs/src/previews/toaster.svelte
+++ docs/src/previews/toaster.svelte
@@ -3,7 +3,7 @@
 	import { fly } from "svelte/transition";
 	import { usePreviewControls } from "@components/preview-ctx.svelte";
 	import Preview from "@components/preview.svelte";
-	import { Toaster, getters } from "melt/builders";
+	import { Progress, Toaster, getters } from "melt/builders";
 	import Close from "~icons/material-symbols/close-rounded";
 
 	const controls = usePreviewControls({
@@ -12,6 +12,13 @@
 			label: "Hover",
 			options: ["pause", "pause-all"],
 			defaultValue: "pause",
+		},
+		closeDelay: {
+			type: "number",
+			min: 0,
+			max: 10000,
+			defaultValue: 5000,
+			label: "Close Delay",
 		},
 	});
 
@@ -40,12 +47,10 @@
 	];
 
 	const toaster = new Toaster<ToastData>({
-		closeDelay: 0,
 		...getters(controls),
 	});
 
 	function addRandomToast() {
-		console.log("add random toast");
 		toaster.addToast({
 			data: toastData[Math.floor(Math.random() * toastData.length)],
 		});
@@ -67,10 +72,12 @@
 		<div
 			class="fixed right-0 top-0 m-4 flex flex-col items-end gap-2 text-left md:bottom-0 md:top-auto"
 		>
-			{#each toaster.toasts as toast (toast.id)}
-				{@const toastBuilder = toaster.getToastFromToaster(toast)}
+			{#each toaster.toasts as t (t.id)}
+				{@const toast = toaster.getToastFromToaster(t)}
+				{@const progress = new Progress({ value: () => toast.percentage })}
+
 				<div
-					{...toastBuilder.content}
+					{...toast.content}
 					animate:flip={{ duration: 500 }}
 					in:fly={{ duration: 150, x: "100%" }}
 					out:fly={{ duration: 150, x: "100%" }}
@@ -78,21 +85,37 @@
 				>
 					<div class="relative w-[24rem] max-w-[calc(100vw-2rem)] gap-4 p-5">
 						<div>
-							<h3 {...toastBuilder.title} class="flex items-center gap-2 !text-base font-semibold">
-								{toastBuilder.data.title}
-								<span class="size-1.5 rounded-full {toastBuilder.data.color}"></span>
+							<h3 {...toast.title} class="flex items-center gap-2 !text-base font-semibold">
+								{toast.data.title}
+								<span class="size-1.5 rounded-full {toast.data.color}"></span>
 							</h3>
-							<div {...toastBuilder.description} class="!m-0 text-base">
-								{toastBuilder.data.description}
+
+							<div {...toast.description} class="!m-0 text-base">
+								{toast.data.description}
 							</div>
 						</div>
+
 						<button
-							{...toastBuilder.close}
+							{...toast.close}
 							aria-label="dismiss alert"
 							class="text-accent-300 absolute right-4 top-4 !m-0 grid size-10 cursor-pointer place-items-center rounded-full bg-gray-800 hover:bg-gray-500/50"
 						>
 							<Close />
 						</button>
+
+						{#if toast.closeDelay !== 0}
+							<div class="absolute bottom-0.5 right-auto">
+								<div
+									{...progress.root}
+									class="relative h-1.5 w-[20rem] overflow-hidden rounded-full bg-white"
+								>
+									<div
+										{...progress.progress}
+										class="h-full w-full translate-x-[var(--progress)] rounded-r-full bg-neutral-700"
+									></div>
+								</div>
+							</div>
+						{/if}
 					</div>
 				</div>
 			{/each}
