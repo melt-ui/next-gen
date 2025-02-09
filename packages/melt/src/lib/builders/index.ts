752101e384891ef6622de46985982d2426b8594e
autriz <explayinformer8519@gmail.com>
2025-02-09 20:19:21 +0400

    remove space

--- packages/melt/src/lib/builders/index.ts
+++ packages/melt/src/lib/builders/index.ts
@@ -11,7 +11,6 @@ export * from "./Slider.svelte";
 export * from "./Tree.svelte";
 export * from "./utils.svelte";
 export * from "./FileUpload.svelte";
-
 export * from "./Tooltip.svelte";
 export * from "./Tree.svelte";
 export * from "./Avatar.svelte";
