111c710e14e27a83fc164a1753bd582cce412373
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-02-11 16:45:26 +0000

    update docs

--- packages/melt/src/lib/types.ts
+++ packages/melt/src/lib/types.ts
@@ -14,3 +14,13 @@ export type ComponentProps<Obj extends Record<string, unknown>> = Omit<
 	WithoutGetters<Obj>,
 	`on${string}Change`
 >;
+
+/**
+ * If `Multiple` is true, it will be an `IterableProp`, which is a `SvelteSet` or a getter that returns an iterable.
+ * If `Multiple` is false, it will be a `MaybeGetter`, which is a value or a getter that returns a value.
+ * @template T - The type of values that can be selected
+ * @template Multiple - Boolean flag indicating if multiple selection is enabled
+ */
+export type MaybeMultiple<T, Multiple extends boolean> = Multiple extends true
+	? IterableProp<T>
+	: MaybeGetter<T | undefined>;
