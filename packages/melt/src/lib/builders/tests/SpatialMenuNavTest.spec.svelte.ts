c219a56497aa8401af37396c5c8866ba728e8c04
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-07-06 15:59:42 +0100

    better tests

---
+++ packages/melt/src/lib/builders/tests/SpatialMenuNavTest.spec.svelte.ts
@@ -0,0 +1,112 @@
+import { page, userEvent } from "@vitest/browser/context";
+import SpatialMenuNavTest from "./SpatialMenuNavTest.svelte";
+import { render } from "vitest-browser-svelte";
+import { testWithEffect } from "$lib/utils/test.svelte";
+import { expect } from "vitest";
+
+type Grid = string[];
+type Direction = "up" | "down" | "left" | "right";
+
+const directionArrowMap: Record<Direction, string> = {
+	up: "ArrowUp",
+	down: "ArrowDown",
+	left: "ArrowLeft",
+	right: "ArrowRight",
+};
+
+type TestCase = {
+	initial: Grid;
+	steps: Array<Grid | Direction>;
+	crossAxis?: boolean;
+	wrap?: boolean;
+};
+
+const cases: TestCase[] = [
+	{
+		// prettier-ignore
+		initial: [
+			"o h o",
+			"o o o",
+		],
+		// prettier-ignore
+		steps: [
+			"right",
+			[
+				"o o h",
+				"o o o"
+			]
+		],
+	},
+	{
+		// prettier-ignore
+		initial: [
+			"o o h",
+			"o o o",
+		],
+		// prettier-ignore
+		steps: [
+			"right",
+			[
+				"o o h",
+				"o o o"
+			]
+		],
+		wrap: false,
+	},
+	{
+		// prettier-ignore
+		initial: [
+			"o o h",
+			"o o o",
+		],
+		// prettier-ignore
+		steps: [
+			"right",
+			[
+				"h o o",
+				"o o o"
+			]
+		],
+		wrap: true,
+	},
+];
+
+function getSupposedHighlightedIdx(grid: Grid): number {
+	const cells = grid.flatMap((r) => r.split(" "));
+	console.log(cells);
+	return cells.findIndex((c) => c === "h");
+}
+
+cases.forEach((c, i) => {
+	testWithEffect(`Navigation test: ${i}`, async () => {
+		const user = userEvent.setup();
+
+		render(SpatialMenuNavTest, { initial: c.initial, wrap: c.wrap, crossAxis: c.crossAxis });
+
+		const root = page.getByTestId("spatial-root");
+		const items = page.getByTestId("spatial-item").all();
+
+		await user.click(root.element());
+		expect(root).toHaveFocus();
+
+		let grid = c.initial;
+		const idx = getSupposedHighlightedIdx(grid);
+		expect(items[idx]!.element().getAttribute("data-highlighted")).toBe("");
+
+		let step_idx = -1;
+		while (grid && step_idx < c.steps.length - 1) {
+			step_idx++;
+			const step = c.steps[step_idx];
+			if (typeof step === "string") {
+				const direction = step as Direction;
+				const arrowKey = directionArrowMap[direction];
+				await user.keyboard(`{${arrowKey}}`);
+			} else {
+				grid = step as Grid;
+				const idx = getSupposedHighlightedIdx(grid);
+				console.log(idx);
+				expect(items[idx]!.element().getAttribute("data-highlighted")).toBe("");
+			}
+		}
+	});
+});
---
+++ packages/melt/src/lib/builders/tests/SpatialMenuNavTest.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+	import { SpatialMenu } from "$lib/builders/SpatialMenu.svelte.js";
+
+	type Props = {
+		initial: string[];
+		wrap?: boolean;
+		crossAxis?: boolean;
+	};
+	const { initial, wrap = false, crossAxis = false }: Props = $props();
+
+	const cols = initial[0]?.split(" ").length ?? 0;
+
+	type Item = {
+		disabled?: boolean;
+		id: string;
+	};
+
+	let highlighted: string | undefined | null = $state();
+
+	const rows: Item[][] = [];
+
+	initial.forEach((row, rowIndex) => {
+		const res: Item[] = [];
+		const cols = row.split(" ");
+		cols.forEach((col, colIndex) => {
+			const id = `${rowIndex}-${colIndex}`;
+			res.push({
+				id,
+				disabled: col === "x",
+			});
+			if (col === "h") {
+				highlighted = id;
+			}
+		});
+		rows.push(res);
+	});
+
+	const menu = new SpatialMenu({
+		highlighted: () => highlighted,
+		onHighlightChange: (id) => {
+			console.log("hey", id);
+			highlighted = id;
+		},
+		wrap,
+		crossAxis,
+	});
+
+	export const getHighlighted = () => highlighted;
+</script>
+
+<div style="grid-template-columns: repeat({cols}, 1fr);" {...menu.root} data-testid="spatial-root">
+	{#each rows as row}
+		{#each row as col}
+			{@const item = menu.getItem(col.id, { disabled: col.disabled })}
+			<div {...item.attrs} data-testid="spatial-item">
+				{col.id}
+			</div>
+		{/each}
+	{/each}
+</div>
+
+<style>
+	[data-melt-spatial-menu-root] {
+		display: grid;
+
+		padding: 0.5rem;
+		border: 1px solid #ccc;
+	}
+
+	[data-melt-spatial-menu-item] {
+		padding: 0.5rem;
+		border: 1px solid #ccc;
+	}
+
+	[data-melt-spatial-menu-item]:hover {
+		background-color: #ddd;
+	}
+
+	[data-melt-spatial-menu-item][data-highlighted] {
+		background-color: #ccc;
+	}
+</style>
